a5be757c584c622a210f6bae217b19533c6d3877
Minnael <minnaelcampelo@gmail.com>
2024-03-06 15:49:29 -0300

    add empresa descriptions

--- src/Components/Pages/Empresa/Empresa.jsx
+++ src/Components/Pages/Empresa/Empresa.jsx
@@ -1,7 +1,21 @@
+import './styles.css'
+
 function Empresa(){
    return (
-      <div>
-         <h1>PAGINA DE EMPRESA</h1>
+      <div className='container-empresa'>
+         <div className='container-descricao'>
+            <h1>QUEM SOMOS</h1>
+            <p><b>VISÃO ESTRATÉGICA:</b> A base de uma empresa de administração de projetos deve ser construída sobre uma visão estratégica robusta. Isso implica definir claramente os objetivos de longo prazo, identificando mercados-alvo e visualizando como a empresa contribuirá para o cenário global de projetos. A visão estratégica é o farol que guiará todas as ações e decisões da empresa.</p>
+            <p><b>COMPROMISSO COM EXCELÊNCIA:</b> Em um ambiente onde a qualidade é o diferencial, a excelência deve ser um valor central. Isso se traduz em entregar projetos dentro do prazo, orçamento e, acima de tudo, atendendo ou superando as expectativas dos clientes. A busca contínua pela melhoria, aprendizado e adaptação a novas práticas são componentes essenciais para manter a excelência.</p>
+            <p><b>TRABALHO EM EQUIPE:</b> Projetos bem-sucedidos não são conquistados isoladamente; eles são o resultado da colaboração eficaz e de equipes coesas. Cultivar um ambiente que promova a comunicação aberta, a troca de ideias e o apoio mútuo entre os membros da equipe é essencial. Uma empresa de administração de projetos deve reconhecer e valorizar a diversidade de habilidades e experiências que cada membro traz.</p>
+         </div>
+
+         <div className='container-descricao'>
+            <h1>NOSSOS VALORES</h1>
+            <p><b>VISÃO ESTRATÉGICA:</b> A base de uma empresa de administração de projetos deve ser construída sobre uma visão estratégica robusta. Isso implica definir claramente os objetivos de longo prazo, identificando mercados-alvo e visualizando como a empresa contribuirá para o cenário global de projetos. A visão estratégica é o farol que guiará todas as ações e decisões da empresa.</p>
+            <p><b>COMPROMISSO COM EXCELÊNCIA:</b> Em um ambiente onde a qualidade é o diferencial, a excelência deve ser um valor central. Isso se traduz em entregar projetos dentro do prazo, orçamento e, acima de tudo, atendendo ou superando as expectativas dos clientes. A busca contínua pela melhoria, aprendizado e adaptação a novas práticas são componentes essenciais para manter a excelência.</p>
+            <p><b>TRABALHO EM EQUIPE:</b> Projetos bem-sucedidos não são conquistados isoladamente; eles são o resultado da colaboração eficaz e de equipes coesas. Cultivar um ambiente que promova a comunicação aberta, a troca de ideias e o apoio mútuo entre os membros da equipe é essencial. Uma empresa de administração de projetos deve reconhecer e valorizar a diversidade de habilidades e experiências que cada membro traz.</p>
+         </div>
       </div>
    )
 }
